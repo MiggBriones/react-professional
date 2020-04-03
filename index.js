7b13e6f3f0c626a89ed171ef94a4679f9aa5369b
Miguel Briones <migg.briones@gmail.com>
2020-04-03 01:42:59 -0600

    Ejemplo para entendimiento de ciclo de vida de un componente. Se implementa Hook useEffect

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 
 const Button = () =>{
-  // useState retorna dos valores, valor actual y el segundo es para actualizarlo
-  const [counter, setCounter] = useState(0);
-
+  const [count, setCount] = useState(0);
+  // funcionalidad que se ejecutará en cada render y puede tener efectos secundarios.
+  useEffect(()=> {
+    console.log("Inicio useEffect");
+    return ()=> {
+      console.log("Fin useEffect")
+    }
+  }, []); // si se pasa una lista vacia a la funcion useEffect, se ejecutará sólo una vez
+  
   return (
-    <div>
-      <p>Presionado {counter} </p>
-      <button onClick={ ()=> setCounter(counter + 1) }>
-        Click me!
-      </button>
-  </div>
+    <button onClick={ ()=> setCount(count+1) }>Click {count}</button>
   )
 }
 
 const App = () =>{
-  return <div> <Button /></div>
+  const [showButton, setShowButton] = useState(true);
+  return (
+    <div>
+      <button onClick={ ()=> setShowButton(false) }>Eliminar botón</button>
+      <div>
+        {showButton && <Button />}
+      </div>
+    </div>
+  );
 }
 
 
