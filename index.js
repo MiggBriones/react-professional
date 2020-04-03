2c28652b4235242e841ed713782d48e13a7c09e2
Miguel Briones <migg.briones@gmail.com>
2020-04-03 01:20:48 -0600

    Ejemplo para asignar valores por defecto al props.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-// componente funcional
-function App3(){
-  return <h1>App</h1>
-}
-const App3 = ()=> <h1>App2</h1>
+const nombres = [
+    'Miguel',
+    'Soler',
+    'Zavala',
+    'Moi',
+    'Papu'
+]
 
+// destructiring, para no tener que acceder al elemento: props.nombre, props.idioma
 
-// componente de clase
-class App extends Component {
-  render() {
-    return <h1>App</h1>
-  }
+// valores por defecto ECMAScript 6.  idioma = 'en'
+const Saludar = ( {nombre, idioma} ) => {
+    const saludo = idioma === 'es' ? 'Hola' : 'Hello';
+    return <p>{saludo} {nombre}</p>
 }
+
+// NOTA. Esta opción no es recomendada, ya en que veriones posteriores se elimibarán. A manera de ejemplo y conocer distintar formas se anexa dicho ejemplo.
+// asignar propiedades a la prop por de defecto
+Saludar.defaultProps = {
+  idioma: 'en'
+}
+const App = () =>{
+  return <div><Saludar nombre='Mike' idioma='en'/> </div>
+}
+
 
 render(<App />, document.getElementById('root'))
