0b33285e5a5f2260f154f37e4e21d3206e64d262
Miguel Briones <migg.briones@gmail.com>
2020-04-03 01:30:06 -0600

    Ejemplo para manejo de estado, se crea un contador para mostrar el valor de cuantas veces ha hecho click el usuario sobre el boton.

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
-const nombres = [
-    'Miguel',
-    'Soler',
-    'Zavala',
-    'Moi',
-    'Papu'
-]
+const Button = () =>{
+  // useState retorna dos valores, valor actual y el segundo es para actualizarlo
+  const [counter, setCounter] = useState(0);
 
-// destructiring, para no tener que acceder al elemento: props.nombre, props.idioma
-
-// valores por defecto ECMAScript 6.  idioma = 'en'
-const Saludar = ( {nombre, idioma} ) => {
-    const saludo = idioma === 'es' ? 'Hola' : 'Hello';
-    return <p>{saludo} {nombre}</p>
+  return (
+    <div>
+      <p>Presionado {counter} </p>
+      <button onClick={ ()=> setCounter(counter + 1) }>
+        Click me!
+      </button>
+  </div>
+  )
 }
 
-// NOTA. Esta opción no es recomendada, ya en que veriones posteriores se elimibarán. A manera de ejemplo y conocer distintar formas se anexa dicho ejemplo.
-// asignar propiedades a la prop por de defecto
-Saludar.defaultProps = {
-  idioma: 'en'
-}
 const App = () =>{
-  return <div><Saludar nombre='Mike' idioma='en'/> </div>
+  return <div> <Button /></div>
 }
 
 
