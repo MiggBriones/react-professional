60dfce0f9f2c60daa3427c4d1d99b2dd4d7f481b
Miguel Briones <migg.briones@gmail.com>
2020-04-03 01:08:46 -0600

    Ejemplo de componente de función y de clase.

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-const nombres = [
-  'Miguel',
-  'Soler',
-  'Zavala',
-  'Moi!!',
-  'Papu'
-]
-
-// nos genera un error al utilizar map: Warning: Each child in a list should have a unique "key" prop.
-// se agrega el key para identificar de manera unica al elemento para actualizarlo
-const Nombres = () =>{
-  return (
-    <ul>{
-    nombres.map( (nombre, index) => <li key={index} >{nombre}</li> )  
-    }</ul>
-  )
+// componente funcional
+function App3(){
+  return <h1>App</h1>
 }
+const App3 = ()=> <h1>App2</h1>
 
-const App = () => {
-  return <h1> <Nombres /> </h1>;
+
+// componente de clase
+class App extends Component {
+  render() {
+    return <h1>App</h1>
+  }
 }
-
 
 render(<App />, document.getElementById('root'))
